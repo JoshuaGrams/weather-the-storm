260573da1bb776b66d59020db51153a74a06e5f1
Josh Grams <josh@qualdan.com>
2021-02-18 16:00:50 -0500

    Show exits after command output.

--- src/story.js
+++ src/story.js
@@ -1,1 +1,8 @@
 UIBar.stow()
+$(document).on(':passagerender', function(ev) {
+	if(ev.passage.tags.includes('nocommands')) return
+	var content = $(ev.content)
+	var links = content.find('a[data-passage]').map((i,e)=>e.dataset.passage).get()
+	State.setVar('_links', links.map(p=>'[['+p+']]').join(', '))
+	content.wiki(Story.get('footer').text)
+})
